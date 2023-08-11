f6e34f41a88ff7fe186e6b80a4e7bb47bfaaecde
Suraj Roy <49346871+8suraj@users.noreply.github.com>
2023-08-12 02:06:52 +0530

    mobile version done (#1)
    Co-authored-by: tkcollabifirst <117096420+tkcollabifirst@users.noreply.github.com>

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,9 @@
+export default function Button() {
+	return (
+		<div className='py-2  p-1 w-full '>
+			<button className='bg-blue-500 text-white text-[.75rem] font-semibold rounded-xl py-[.89rem]    w-full'>
+				Next
+			</button>
+		</div>
+	);
+}
